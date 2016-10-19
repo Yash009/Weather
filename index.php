c0e182ea9507934d48d4838c49308108d7a75d0a
Yash009 <yash.900209@gmail.com>
2016-10-19 15:53:55 -0400

    debug4

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ header('Content-Type: text/xml');
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 $city="$_Post[Body]";
 $output;
-//$country="IN"; //Two digit country code$url="http://api.openweathermap.org/data/2.5/weather?APPID=eb8d1e4d0ede56c4af30df4e8729e52e&q=".$city."&units=metric&lang=en";
+//$country="IN";
+//Two digit country code
+$url="http://api.openweathermap.org/data/2.5/weather?APPID=eb8d1e4d0ede56c4af30df4e8729e52e&q=".$city."&units=metric&lang=en";
 $json=file_get_contents($url);
 $data=json_decode($json,true);
 //Get current Temperature in Celsius
