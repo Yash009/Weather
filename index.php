c5ebafbd1f15e57e9f913b77d5b3144323889f28
Yash009 <yash.900209@gmail.com>
2016-10-19 15:44:08 -0400

    debug2

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: text/xml');
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-$response->sms("Enter your city");
+$city="$_Post[Body]";
 $output;
 //$country="IN"; //Two digit country code$url="http://api.openweathermap.org/data/2.5/weather?APPID=eb8d1e4d0ede56c4af30df4e8729e52e&q=".$city."&units=metric&lang=en";
 $json=file_get_contents($url);
