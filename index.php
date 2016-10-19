b8368f84e6a017a65b309bbae415ddf1c7d84f7c
Yash009 <yash.900209@gmail.com>
2016-10-19 17:28:16 -0400

    debug7

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <?php
 header('Content-Type: text/xml');
-//echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?
+echo "<?xml version='1.0' encoding='UTF-8'?>";
 $city=$_POST['Body'];
 //$country="IN";
 //Two digit country code
 $url="http://api.openweathermap.org/data/2.5/weather?APPID=eb8d1e4d0ede56c4af30df4e8729e52e&q=".$city."&units=metric&lang=en";
 $json=file_get_contents($url);
 $data=json_decode($json,true);
-//Get current Temperature in Celsius
-//var_dump($data);
-$output[] = $output.$data['main']['temp'];
-//Get weather condition
-$output[] = $output.$data['weather'][0]['main'];
-//Get cloud percentage
-$output[] = $output.$data['clouds']['all'];
-//Get wind speed
-$output[] = $output.$data['wind']['speed'];
+$output = "Temperature: ".$output.$data['main']['temp']." C, ".$output.$data['weather'][0]['description'].", Humidity: ".$output.$data['main']['humidity'].", Wind Speed: ".$output.$data['wind']['speed']."kmph";
+
 ?>
 <Response>
     <Sms>
